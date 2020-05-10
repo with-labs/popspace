c4b0b02fd66603c0b3af7fd4f8ab3ee8dbbeefd2
jtyler-dev <jtyler.dev@gmail.com>
2020-05-09 17:55:37 -0700

    motion animation upgrade

---
+++ src/withComponents/SlideMenu/SlideMenu-animation.ts
@@ -0,0 +1,84 @@
+export const slideBtnCherryVariants = {
+  closed: {
+    x: 16,
+    y: 16,
+    width: 8,
+    height: 8,
+    borderRadius: 2,
+  },
+  open: {
+    x: 24,
+    y: 12,
+    width: 4,
+    height: 12,
+  },
+};
+
+export const slideBtnSageVariants = {
+  closed: {
+    x: 28,
+    y: 16,
+    width: 8,
+    height: 8,
+    borderRadius: 2,
+  },
+  open: {
+    x: 28,
+    y: 24,
+    width: 12,
+    height: 4,
+  },
+};
+
+export const slideBtnTangerineVariants = {
+  closed: {
+    x: 16,
+    y: 28,
+    width: 8,
+    height: 8,
+    borderRadius: 2,
+  },
+  open: {
+    x: 12,
+    y: 24,
+    width: 12,
+    height: 4,
+  },
+};
+
+export const slideBtnBlueberryVariants = {
+  closed: {
+    x: 28,
+    y: 28,
+    width: 8,
+    height: 8,
+    borderRadius: 2,
+  },
+  open: {
+    x: 24,
+    y: 28,
+    width: 4,
+    height: 12,
+  },
+};
+
+export const btn = {
+  width: 52,
+  height: 52,
+  backgroundColor: '#fff',
+  borderRadius: 16,
+};
+
+export const btnActive = {
+  rotation: 135,
+  borderRadius: 30,
+};
+
+export const slideMenuVariants = {
+  closed: {
+    x: 150,
+  },
+  open: {
+    x: 0,
+  },
+};
--- src/withComponents/SlideMenu/SlideMenu.tsx
+++ src/withComponents/SlideMenu/SlideMenu.tsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import clsx from 'clsx';
 
 import { ReactComponent as Add } from '../../images/icons/add.svg';
 import { ReactComponent as AddRotated } from '../../images/icons/add_rotated.svg';
+
+import useWindowSize from '../../withHooks/useWindowSize/useWindowSize';
+
+import {
+  slideBtnSageVariants,
+  slideBtnCherryVariants,
+  slideBtnTangerineVariants,
+  slideBtnBlueberryVariants,
+  slideMenuVariants,
+} from './SlideMenu-animation';
 
 import './slideMenu.css';
 
@@ -31,6 +42,7 @@ export const SlideMenu: React.FC<SlideMenuProps> = props => {
   } = props;
 
   const dropDownMenuRef = useRef<HTMLDivElement>(null);
+  const [windowWidth, windowHeight] = useWindowSize();
 
   useEffect(() => {
     const dropRefCurrent = dropDownMenuRef.current;
@@ -79,24 +91,74 @@ export const SlideMenu: React.FC<SlideMenuProps> = props => {
     }
   };
 
+  // const slideRightMenu = (
+  //   <div className="u-sm-displayNone u-md-flex u-lg-flex">
+  //     <div
+  //       className={clsx('SlideMenu-button u-layerControlsBeta', {
+  //         'is-open': isActive,
+  //       })}
+  //       onClick={onButtonClick}
+  //     >
+  //       <Add />
+  //     </div>
+  //     <div
+  //       className={clsx('SlideMenu-menu', {
+  //         'is-open': isActive,
+  //       })}
+  //     >
+  //       {children}
+  //     </div>
+  //   </div>
+  // );
+
   const slideRightMenu = (
-    <div className="u-sm-displayNone u-md-flex u-lg-flex">
-      <div
-        className={clsx('SlideMenu-button u-layerControlsBeta', {
+    <motion.div className="u-sm-displayNone u-md-flex u-lg-flex" animate={isActive ? 'open' : 'closed'} initial={false}>
+      <motion.div
+        initial={false}
+        variants={{
+          closed: { x: 0, rotate: 0 },
+          open: { x: -265, rotate: 135 },
+        }}
+        className={clsx('SlideMenu-button u-layerControlsBeta u-positionRelative', {
           'is-open': isActive,
         })}
         onClick={onButtonClick}
       >
-        <Add />
-      </div>
-      <div
-        className={clsx('SlideMenu-menu', {
-          'is-open': isActive,
-        })}
+        <motion.div
+          initial={false}
+          className="SlideMenu-btnIcon--cherry u-positionAbsolute"
+          variants={slideBtnCherryVariants}
+        />
+        <motion.div
+          initial={false}
+          className="SlideMenu-btnIcon--sage u-positionAbsolute"
+          variants={slideBtnSageVariants}
+        />
+        <motion.div
+          initial={false}
+          className="SlideMenu-btnIcon--tangerine u-positionAbsolute"
+          variants={slideBtnTangerineVariants}
+        />
+        <motion.div
+          initial={false}
+          className="SlideMenu-btnIcon--blueberry u-positionAbsolute"
+          variants={slideBtnBlueberryVariants}
+        />
+      </motion.div>
+      <motion.div
+        className="SlideMenu-menu"
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: 320,
+          },
+        }}
       >
         {children}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 
   const mobileSlideMenu = (

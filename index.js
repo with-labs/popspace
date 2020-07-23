63258844a545a01dcfac3e1a976c7d3960730cbe
Alexey <iWizem@gmail.com>
2020-07-23 00:30:11 -0400

    Barebones express app

---
+++ index.js
@@ -0,0 +1,10 @@
+require("./init/init_global")
+
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => res.send('non-api Hello World!'))
+app.get('/api/*', (req, res) => res.send('api Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
---
+++ init/init_global.js
@@ -0,0 +1,4 @@
+if(process.env.NODE_ENV == "development") {
+  require("dotenv").config()
+}
+

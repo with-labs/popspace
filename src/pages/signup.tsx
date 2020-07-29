b88e36d158d610e2b5d06ed26b42f58f7ec085f7
Alexey <iWizem@gmail.com>
2020-07-29 00:49:30 -0400

    Add create_account endpoint, send XHR request to it from signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
+import Api from '../utils/api';
 
 const Main = styled('main')({
   height: '100%',
@@ -8,6 +9,114 @@ const Main = styled('main')({
   textAlign: 'center',
 });
 
-export default function Landing() {
-  return <Main>Sign up</Main>;
+const FieldHeader = styled('h1')({});
+const FieldInput = styled('input')({
+  fontSize: '24px',
+});
+
+interface Data {
+  key: string;
+  value: any;
+}
+
+// https://medium.com/@colebemis/building-a-checkbox-component-with-react-and-styled-components-8d3aa1d826dd
+// TODO: Maybe move out to reusable component
+const Checkbox = (props: any) => <input type="checkbox" {...props} />;
+
+const LabeledCheckbox = (props: any) => (
+  <label>
+    <Checkbox id={props.id} defaultChecked={props.checked} onChange={props.onCheckboxChange} />
+    <span>{props.text}</span>
+  </label>
+);
+
+export default function Signup(props: any) {
+  const checkboxesState = {
+    accept_tos: false,
+    receive_marketing: true,
+  };
+
+  const onCheckboxChange = (event: any) => {
+    console.log(event.target.id);
+    const checkbox = event.target;
+    const id = event.target.id;
+  };
+
+  const finishSignup = async () => {
+    const input = getInput();
+    const validation = checkInput(input);
+    if (validation.valid) {
+      const result = await Api.signup(input);
+      console.log(result);
+    } else {
+      console.log(validation);
+    }
+  };
+
+  const getInputValue = (fieldId: string) => {
+    const input = document.getElementById(fieldId) as HTMLInputElement;
+    return input.value;
+  };
+
+  const getCheckedValue = (checkboxId: string) => {
+    const checked = document.getElementById(checkboxId) as HTMLInputElement;
+    return checked.checked;
+  };
+
+  const getInput = () => {
+    return {
+      firstName: getInputValue('first_name'),
+      lastName: getInputValue('last_name'),
+      email: getInputValue('email'),
+      acceptTos: getCheckedValue('accept_tos'),
+      receiveMarketing: getCheckedValue('receive_marketing'),
+    };
+  };
+
+  const checkInput = (input: { [key: string]: any }) => {
+    const invalidFields: any = {};
+    // TODO: we can hook up some npm package for field validation
+    if (!input['firstName']) invalidFields['first_name'] = true;
+    if (!input['lastName']) invalidFields['last_name'] = true;
+    // We don't need to validate email too much, since we'll verify it via otp
+    if (!input['email']) invalidFields['email'] = true;
+    if (!input['acceptTos']) invalidFields['accept_tos'] = true;
+
+    if (Object.keys(invalidFields).length > 0) {
+      return {
+        valid: false,
+        invalidFields: invalidFields,
+      };
+    } else {
+      return { valid: true };
+    }
+  };
+
+  return (
+    <Main>
+      <FieldHeader>First name</FieldHeader>
+      <FieldInput id="first_name"></FieldInput>
+      <FieldHeader>Last name</FieldHeader>
+      <FieldInput id="last_name"></FieldInput>
+      <FieldHeader>Email</FieldHeader>
+      <FieldInput id="email"></FieldInput>
+      <div>
+        <LabeledCheckbox
+          id="accept_tos"
+          checked={checkboxesState['accept_tos']}
+          onCheckboxChange={onCheckboxChange}
+          text="Accept TOS"
+        />
+      </div>
+      <div>
+        <LabeledCheckbox
+          id="receive_marketing"
+          checked={checkboxesState['receive_marketing']}
+          onCheckboxChange={onCheckboxChange}
+          text="Marketing"
+        />
+      </div>
+      <button onClick={finishSignup}> Sign up </button>
+    </Main>
+  );
 }
---
+++ src/utils/api.ts
@@ -0,0 +1,24 @@
+class Api {
+  constructor() {}
+
+  async signup(data: any) {
+    return await this.post('/create_account', data);
+  }
+
+  async post(endpoint: string, data: any) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `/.netlify/functions${endpoint}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    return new Promise((resolve, reject) => {
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          console.log(xhr.response);
+          resolve(JSON.parse(xhr.response));
+        }
+      };
+      xhr.send(JSON.stringify(data));
+    });
+  }
+}
+
+export default new Api();

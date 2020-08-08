0b17a9617272091f61660942bd63a709738036f3
Alexey <iWizem@gmail.com>
2020-08-08 18:13:57 -0400

    Add build script that that creates a .env from build-time env vars
    As suggested in https://community.netlify.com/t/support-guide-using-environment-variables-on-netlify-correctly/267/5
    We'd like to be aware of the environment at run-time, not only at build-time
    However, netlify only supports build-time env vars in the toml file,
    and only one set of env vars in the admin panel
    So the workaround is to dump some of the .toml env vars into .env,
    and load it with dotenv.

---
+++ scripts/build.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const cmd = require('shelljs').exec
+
+
+// https://community.netlify.com/t/support-guide-using-environment-variables-on-netlify-correctly/267/5
+// The intention is to make some of the build-time nev vars of netlify available at runtime.
+// Netlify only allows build-time env vars via netlify.toml, and global env vars,
+// which do not distinguish between different build environments.
+// The toml file allows distinguishing between environments, but is not available at runtime.
+// So at build time, we dump the env vars into a .env file, and load it with dotenv.
+fs.writeFileSync(
+  './.env',
+  `NODE_ENV=${process.env.NODE_ENV}\n`
+);
+
+cmd("cross-env CI=false REACT_APP_SET_AUTH=passcode react-scripts build", (err, msg) => {
+  if(err) {
+    return console.log(err)
+  }
+})

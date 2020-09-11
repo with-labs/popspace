4515ae68b97da4b504fdc9b2e0fadc323fc7e90c
jtyler <jesse@with.so>
2020-09-11 15:39:42 -0700

    wip

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './Admin.module.css';
+import Api from '../../utils/api';
+
+import { Header } from '../../withComponents/Header/Header';
+
+interface IAdminProps {}
+
+export const Admin: React.FC<IAdminProps> = (props) => {
+  const onFormSubmit = () => {};
+
+  return <main></main>;
+};
--- src/withComponents/Header/Header.tsx
+++ src/withComponents/Header/Header.tsx
@@ -4,12 +4,21 @@ import styles from './Header.module.css';
 
 import { ReactComponent as WithLogo } from '../../images/logo/medium.svg';
 
-interface IHeaderProps {}
+interface IHeaderProps {
+  displayLogin?: boolean;
+}
 
 export const Header: React.FC<IHeaderProps> = (props) => {
+  const { displayLogin } = props;
+  const loginButton = displayLogin ? (
+    <div className="u-flex u-flexRow">
+      <div></div>
+    </div>
+  ) : null;
   return (
     <header className={clsx(styles.header, 'u-flex u-flexAlignItemsCenter')}>
       <WithLogo />
+      {loginButton}
     </header>
   );
 };

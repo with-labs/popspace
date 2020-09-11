a714ef561f0afd9fbc45a475d8f99dbf1186925e
Alexey <iWizem@gmail.com>
2020-09-11 01:34:01 -0400

    Add priority level to room_names

--- functions/lib/db/pg/migrations/1599798367890_create-room-names.js
+++ functions/lib/db/pg/migrations/1599798367890_create-room-names.js
@@ -6,13 +6,16 @@ exports.up = pgm => {
   pgm.createTable('room_names', {
     id: { type: 'bigserial', primaryKey: true },
     room_id: { type: 'bigint', notNull: true },
-    name: { type: 'text', notNull: true },
+    name: { type: 'text', notNull: true, unique: true},
+    // Allows chosing a most preferred name for a room with many names
+    priority_level: { type: 'integer', notNull: true, default: 0 },
     created_at: {
       type: 'timestamptz',
       notNull: true,
       default: pgm.func("(now() at time zone 'utc')"),
     }
   })
+  pgm.createIndex('room_names', ['room_id', 'priority_level'])
   pgm.createIndex('room_names', 'name')
 };
 

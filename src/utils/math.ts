3d0ca1de4bb120417ca3db4a6b9030c932595adc
Grant Forrest <grant@with.so>
2020-10-06 16:03:34 -0400

    fix distance algorithm

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -32,5 +32,5 @@ export function roundVector(vector: Vector2) {
 }
 
 export function vectorDistance(v1: Vector2, v2: Vector2) {
-  return Math.sqrt(Math.pow(Math.abs(v1.x - v1.y), 2) + Math.pow(Math.abs(v1.y - v2.y), 2));
+  return Math.sqrt(Math.pow(Math.abs(v1.x - v2.x), 2) + Math.pow(Math.abs(v1.y - v2.y), 2));
 }

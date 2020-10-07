01aa90217a96baafd759388e17b76bbcae3b8217
Alexey <iWizem@gmail.com>
2020-10-07 17:15:28 -0400

    Claim room email copy (#242)
    Better copy for claim room email

--- functions/lib/email/room.js
+++ functions/lib/email/room.js
@@ -10,35 +10,37 @@ module.exports = {
   },
 
   sendRoomClaimEmail: async (toEmail, roomName, url) => {
-    const subject = `Claim your room {$roomName}`
+    const subject = `Claim your room ${roomName}`
     const tags = [{Name: "type", Value: "room_claim"}]
 
     const html = `
-    <body style="padding: 0px !important; margin: 0px !important; background: white;">
-      <div style="padding:40px; margin:0px; background: white; max-width: 520px; margin: 0px auto; text-align: center;">
-        <a href="https://with.so" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" class="x_logo-img" style="border:none; display:block; height:34px; text-decoration:none; margin-bottom: 10px;">
-        <img data-imagetype="External" src="https://with.so/images/logo_medium.svg" alt="" width="auto" height="28" border="0" />
-      </a>
-        <img src="https://s3.us-east-2.amazonaws.com/with.static/02.jpg" alt="A girl holding an envelope over her head with a happy face." style="width: 100%; height: auto;" />
-        <div style="padding:40px; margin:0px; background: #FFF8F0; margin: 0px auto; text-align: left;">
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
-            Hi,</p>
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
-            At With, we are hard at work helping your team make great things together.</p>
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
-            It's time to migrate to our new user system! This will enable new features coming soon like the ability to create additional rooms, invite team members via email, and preserve the contents of your room between visits, in a secure and convenient way.</p>
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">To migrate please use this link to associate your room <b>${roomName}</b> with your account <b>${toEmail}</b>.</p>
-          <div style="margin-top:30px!important; margin-bottom:30px!important;"><a href="${url}" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" style="background-color:#ffd873; border-radius:6px; border:1px solid #ebc76a; box-shadow: 0px 2px 8px 0px #ededed; color:#333; display:inline-block; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:16px; font-weight:700; line-height:40px; padding:0 16px; text-align:center; text-decoration:none">Claim my room ${roomName}</a></div>
-          <p style="font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:13px; line-height:1.6!important; color:#333; margin:0px!important;">Or just copy the link below and paste it into your preferred browser.</p>
-          <div style="border-radius:6px; margin-top:10px!important; padding: 12px; background: #ffffff; overflow-wrap: break-word; word-wrap: break-word; -ms-word-break: break-all; word-break: break-word; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:13px; line-height:1.6!important; color:#333; margin-bottom:30px!important;">${url}</div>
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">Please feel free to reply to this email if you have any questions.</p>
-          <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">Cheers,<br />The With team</p>
+      <body style="padding: 0px !important; margin: 0px !important; background: white;">
+        <div style="padding:40px; margin:0px; background: white; max-width: 520px; margin: 0px auto; text-align: center;">
+            <a href="https://with.so" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" class="x_logo-img" style="border:none; display:block; height:34px; text-decoration:none; margin-bottom: 10px;">
+            <img src="https://s3.us-east-2.amazonaws.com/with.static/logo.png" width="68" height="30" border="0" />
+          </a>
+          <div style="padding:0px; background: #FFF8F0; margin: 0px auto; text-align: left;">
+            <img src="https://s3.us-east-2.amazonaws.com/with.static/02.jpg" alt="A girl holding an envelope over her head with a happy face." style="width: 100%; height: auto;" />
+            <div style="padding:40px;">
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
+                Hi,</p>
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
+                At With, we are hard at work helping your team make great things together.</p>
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">
+                It's time to migrate to our new user system! This will enable new features coming soon like the ability to create additional rooms, invite team members via email, and preserve the contents of your room between visits in a secure and convenient way.</p>
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">To migrate please use this link to associate your room <b>${roomName}</b> with your account <b>${toEmail}</b>.</p>
+            <div style="margin-top:30px!important; margin-bottom:30px!important;"><a href="${url}" target="_blank" rel="noopener noreferrer" data-auth="NotApplicable" style="background-color:#ffd873; border-radius:6px; border:1px solid #ebc76a; box-shadow: 0px 2px 8px 0px #ededed; color:#333; display:inline-block; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:16px; font-weight:700; line-height:24px; padding:8px 16px; text-align:center; text-decoration:none">Claim my room ${roomName}</a></div>
+            <p style="font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:13px; line-height:1.6!important; color:#333; margin:0px!important;">Or just copy the link below and paste it into your preferred browser.</p>
+            <div style="border-radius:6px; margin-top:10px!important; padding: 12px; background: #ffffff; overflow-wrap: break-word; word-wrap: break-word; -ms-word-break: break-all; word-break: break-word; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:13px; line-height:1.6!important; color:#333; margin-bottom:30px!important;">${url}</div>
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">Please feel free to reply to this email if you have any questions.</p>
+            <p style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif !important; font-size:16px; line-height:1.6 !important; color: #333; margin: 0px 0px 10px 0px !important">Cheers,<br />The With team</p>
+            </div>
+          </div>
+          <div style="margin-top:20px!important; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:10px; line-height:1.6!important; color:#aaa;">
+            <b>With Labs, Inc.</b><br />595 Pacific Ave, 4th Floor<br /> San Francisco, CA 94133
+          </div>
         </div>
-        <div style="margin-top:20px!important; font-family:'Helvetica Neue',Helvetica,Arial,sans-serif!important; font-size:10px; line-height:1.6!important; color:#aaa;">
-          <b>With Labs, Inc.</b><br />595 Pacific Ave, 4th Floor<br /> San Francisco, CA 94133
-        </div>
-      </div>
-    </body>
+      </body>
     `
 
     const plaintextFallback = `Hi,\n

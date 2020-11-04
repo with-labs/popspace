b63050039797d4b3ac43c9aec4c1fd561450a76d
Grant Forrest <71781117+grant-with@users.noreply.github.com>
2020-11-04 17:04:27 -0500

    fix video corner radius in safari (#371)
    - fixed a bug where video corners extended beyond the frame

--- src/features/room/people/PersonBubble.tsx
+++ src/features/room/people/PersonBubble.tsx
@@ -52,6 +52,7 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
     objectFit: 'cover',
+    borderRadius: theme.shape.borderRadius,
   },
   audio: {
     display: 'none',

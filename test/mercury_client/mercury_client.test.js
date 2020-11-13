9af566c132c7f018600ebf342c7b944e46716d4b
Alexey <iWizem@gmail.com>
2020-11-13 03:02:01 -0500

    Test with back and forth between 2 clients

--- test/mercury_client/mercury_client.test.js
+++ test/mercury_client/mercury_client.test.js
@@ -19,4 +19,20 @@ describe('mercury_client', () => {
       expect(message).toEqual(sentMessage)
     })
   })
+
+  test('communication between 2 clients', async () => {
+    const messages1 = ["hello", "1", "hi", "3"]
+    const messages2 = ["goodbye", "2", "bye", "4"]
+    const expectedSequence =[
+      messages1[0], messages2[0],
+      messages1[1], messages2[1],
+      messages1[2], messages2[2],
+      messages1[3], messages2[3],
+    ]
+    // Send copies of the arrays since we want to check the results against them
+    const exchangeResult = await scenarios["2_peers_back_and_forth"]([...messages1], [...messages2])
+    expect(exchangeResult.received1).toEqual(messages2)
+    expect(exchangeResult.received2).toEqual(messages1)
+    expect(exchangeResult.sequence).toEqual(expectedSequence)
+  })
 })
--- test/mercury_client/mercury_client_scenarios.js
+++ test/mercury_client/mercury_client_scenarios.js
@@ -2,41 +2,42 @@ const tlib = require("../lib/_testlib")
 const Client = require("../../src/client/client")
 const Mercury = require("../../src/mercury")
 
+const begin = async (clientCount) => {
+  const mercury = new Mercury(process.env.EXPRESS_PORT)
+  mercury.start()
+  const clients = []
+  for(let i = 0; i < clientCount; i++) {
+    clients.push(new Client(`ws://localhost:${process.env.EXPRESS_PORT}`))
+  }
+  await Promise.all( clients.map((c) => (c.connect())) )
+  return { clients, mercury }
+}
+
+const finish = async (mercury, clients) => {
+  await mercury.stop()
+  return await Promise.all(tlib.util.closePromises(clients))
+}
+
 module.exports = {
   "connect_send_disconnect": async() => {
-    const mercury = new Mercury(process.env.EXPRESS_PORT)
-    mercury.start()
-    const client = new Client(`ws://localhost:${process.env.EXPRESS_PORT}`);
-    await client.connect()
-    client.send("hello")
-    await mercury.stop()
-    return await Promise.all(tlib.util.closePromises([client]))
+    const { clients, mercury } = await begin(1)
+    clients[0].send("hello")
+    return await finish(mercury, clients)
   },
 
   "1_sender_2_receivers": async (message='hello') => {
-    const mercury = new Mercury(process.env.EXPRESS_PORT)
-    mercury.start()
     /*
       1. Open several clients
       2. Send a message from one of them
       3. Make sure all clients except sender receive the message
     */
-    const clients = [
-      new Client(`ws://localhost:${process.env.EXPRESS_PORT}`),
-      new Client(`ws://localhost:${process.env.EXPRESS_PORT}`),
-      new Client(`ws://localhost:${process.env.EXPRESS_PORT}`),
-    ]
-
-    await Promise.all( clients.map((c) => (c.connect())) )
+    const { clients, mercury } = await begin(3)
 
     const sender = clients[0]
-    const sentMessage = "hello"
-
     const messagesReceived = []
     const receivePromises = []
 
     clients.forEach((client) => {
-
       if(client != sender) {
         receivePromises.push(new Promise((resolve, reject) => {
           client.on('message', (message) => {
@@ -45,15 +46,56 @@ module.exports = {
           })
         }))
       }
-
     })
 
-    clients[0].send(sentMessage)
+    sender.send(message)
     await Promise.all(receivePromises)
-
-    await mercury.stop()
-    await Promise.all(tlib.util.closePromises(clients))
-
+    await finish(mercury, clients)
     return messagesReceived
+  },
+
+  "2_peers_back_and_forth": async (messageList1=["hello", "1"], messageList2=["goodbye", "2"]) => {
+    const { clients, mercury } = await begin(2)
+
+    const messagesReceived1 = []
+    const messagesReceived2 = []
+    const sequence = []
+
+    await new Promise((resolve, reject) => {
+      clients[0].on('message', (message) => {
+        messagesReceived1.push(message)
+        sequence.push(message)
+        if(messageList1.length > 0) {
+          clients[0].send(messageList1.shift())
+        } else {
+          resolve()
+        }
+      })
+
+      clients[1].on('message', (message) => {
+        messagesReceived2.push(message)
+        sequence.push(message)
+        if(messageList2.length > 0) {
+          clients[1].send(messageList2.shift())
+        } else {
+          resolve()
+        }
+      })
+
+      clients[0].send(messageList1.shift())
+    })
+
+    await finish(mercury, clients)
+
+    return {
+      received1: messagesReceived1,
+      received2: messagesReceived2,
+      sequence: sequence
+    }
+
+
+
+
+
   }
 }

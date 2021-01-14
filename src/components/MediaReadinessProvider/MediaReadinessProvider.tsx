e6fcecf3372d19890251ed8a5a3d35e9be31a1ed
Grant Forrest <gaforres@gmail.com>
2021-01-14 10:06:01 -0500

    reset media ready state on mobile tab hide (#481)
    Co-authored-by: Grant Forrest <grant@with.so>

--- src/components/MediaReadinessProvider/MediaReadinessProvider.tsx
+++ src/components/MediaReadinessProvider/MediaReadinessProvider.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { isMobileOnly } from 'react-device-detect';
 
 export const MediaReadinessContext = React.createContext({
   isReady: false,
@@ -21,6 +22,24 @@ export const MediaReadinessProvider: React.FC = ({ children }) => {
     }),
     [isReady, onReady]
   );
+
+  React.useEffect(() => {
+    if (isMobileOnly) {
+      // on mobile, we need to reset the readiness state when the user backgrounds
+      // the browser tab, since an unfocused tab closes media contexts and requires
+      // a new gesture to activate again
+      const handleVisibilityChange = () => {
+        if (document.visibilityState === 'hidden') {
+          setIsReady(false);
+        }
+      };
+
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+      return () => {
+        document.removeEventListener('visibilitychange', handleVisibilityChange);
+      };
+    }
+  }, []);
 
   return <MediaReadinessContext.Provider value={value}>{children}</MediaReadinessContext.Provider>;
 };

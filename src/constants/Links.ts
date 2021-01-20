2cf3af230d358ad2b500fa3cd0c5fcff606f6885
jesse-with <68447170+jesse-with@users.noreply.github.com>
2021-01-20 10:21:48 -0800

    updating tos and privacy urls (#501)

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -1,7 +1,7 @@
 // file contains links we need from the landing page
 export enum Links {
   LANDING_PAGE = 'https://www.with.so/',
-  TOS = 'https://www.with.so/terms.html',
-  PRIVACY_POLICY = 'https://www.with.so/privacy.html',
+  TOS = 'https://with.so/terms-of-service',
+  PRIVACY_POLICY = 'https://with.so/privacy-policy',
   FEEDBACK = 'https://feedback.with.so',
 }

aa99ad966783e77e275804c9e0ef58ff4ebbcb0e
Grant Forrest <gaforres@gmail.com>
2021-03-05 11:43:55 -0500

    lower resolution for video (#599)

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ export const DEFAULT_VIDEO_CONSTRAINTS: MediaTrackConstraints = {
   // fix for Chromium bug #1084702: video size must be a
   // multiple of 16 to avoid corruption on Pixel 3(a)
   // https://bugs.chromium.org/p/chromium/issues/detail?id=1084702
-  width: 1024,
+  width: 320,
   // our videos are square, so the capture dimensions are also square
-  height: 1024,
+  height: 320,
   frameRate: 24,
 };

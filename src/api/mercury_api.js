333cee984187eb9f8d877c539ab3b99d38659f15
Grant Forrest <gaforres@gmail.com>
2021-03-30 13:24:52 -0400

    add endpoints to modify user default room

--- src/api/mercury_api.js
+++ src/api/mercury_api.js
@@ -1,6 +1,6 @@
 const Api = require("./api")
 const http = require("./http")
-const routes = require('./routes')
+const routes = require("./routes")
 
 class MercuryApi {
   constructor(mercury) {
@@ -23,7 +23,7 @@ class MercuryApi {
 
     this.api.memberOrOwnerRoomRouteEndpoint("/get_public_invite_details", async (req, res) => {
       const routeEntries = await shared.db.room.invites.getActivePublicInviteUrls(req.room.id)
-      const inviteDetails = routeEntries.map((entry) => ({inviteId: entry.id, otp: entry.otp} ))
+      const inviteDetails = routeEntries.map((entry) => ({ inviteId: entry.id, otp: entry.otp }))
       return http.succeed(req, res, { inviteDetails })
     })
 
@@ -35,20 +35,20 @@ class MercuryApi {
       */
       const otp = req.body.otp
       const inviteId = req.body.invite_id
-      if(!inviteId) {
-        return http.fail(req, res, "Must provide inviteId", {errorCode: shared.error.code.INVALID_API_PARAMS})
+      if (!inviteId) {
+        return http.fail(req, res, "Must provide inviteId", { errorCode: shared.error.code.INVALID_API_PARAMS })
       }
       const invite = await shared.db.room.invites.inviteById(inviteId)
-      if(!invite) {
-        return http.fail(req, res,  "No such invite", { errorCode: shared.error.code.INVALID_INVITE })
+      if (!invite) {
+        return http.fail(req, res, "No such invite", { errorCode: shared.error.code.INVALID_INVITE })
       }
       const resolve = await shared.db.room.invites.joinRoomThroughPublicInvite(invite, req.user, otp)
-      if(resolve.error) {
-        if(resolve.error == shared.error.code.JOIN_ALREADY_MEMBER) {
+      if (resolve.error) {
+        if (resolve.error == shared.error.code.JOIN_ALREADY_MEMBER) {
           const roomNameEntry = await shared.db.rooms.preferredNameById(invite.room_id)
           return http.succeed(req, res, { roomRoute: roomNameEntry.name })
         }
-        return http.fail(req, res, "Unable to become member.", { errorCode: resolve.error} )
+        return http.fail(req, res, "Unable to become member.", { errorCode: resolve.error })
       }
       const roomNameEntry = await shared.db.rooms.preferredNameById(invite.room_id)
       return http.succeed(req, res, { roomRoute: roomNameEntry.name })
@@ -65,8 +65,59 @@ class MercuryApi {
       const result = await shared.db.room.invites.disablePublicInviteUrl(req.room.id)
       return http.succeed(req, res)
     })
-  }
 
+    this.api.memberOrOwnerRoomRouteEndpoint("/set_default_room", async (req, res) => {
+      await shared.db.pg.massive.default_rooms.save({
+        user_id: req.user.id,
+        room_id: req.room.id,
+      })
+      return http.succeed(req, res)
+    })
+
+    this.api.loggedInPostEndpoint("/get_or_init_default_room", async (req, res) => {
+      let defaultRoom = await shared.db.pg.massive.default_rooms.findOne({
+        user_id: req.user.id,
+      })
+
+      // if no default_room row exists, we fallback to a heuristic -
+      // choose an arbitrary owned room, if no owned rooms choose an arbitrary
+      // membership room.
+      if (!defaultRoom) {
+        const firstOwnedRoom = await shared.db.pg.massive.rooms.findOne({
+          owner_id: req.user.id,
+        })
+        if (firstOwnedRoom) {
+          defaultRoom = {
+            user_id: req.user.id,
+            room_id: firstOwnedRoom.id,
+          }
+        } else {
+          const firstRoomMembership = await shared.db.pg.massive.room_memberships.findOne({
+            user_id: req.user.id,
+          })
+          if (firstRoomMembership) {
+            return http.fail(req, res, lib.ErrorCodes.NO_DEFAULT_ROOM)
+          }
+          defaultRoom = {
+            user_id: req.user.id,
+            room_id: firstRoomMembership.room_id,
+          }
+        }
+        // write the chosen default so it remains stable for future requests
+        await shared.db.pg.massive.default_rooms.insert(defaultRoom)
+      }
+
+      const preferredRoute = await shared.db.rooms.latestMostPreferredRouteEntry(defaultRoom.room_id)
+      if (!preferredRoute) {
+        // TODO: log this, if a room doesn't have a route that represents a major problem in room setup
+        return http.fail(req, res, "Unexpected error getting room information", {
+          errorCode: lib.ErrorCodes.UNEXPECTED_ERROR,
+        })
+      }
+      const preferredRouteName = preferredRoute.name
+      return http.succeed(req, res, { room_route: preferredRouteName })
+    })
+  }
 }
 
 module.exports = MercuryApi
--- src/lib/log.js
+++ src/lib/log.js
@@ -1,43 +1,43 @@
-const log4js = require('log4js')
+const log4js = require("log4js")
 const log4jsConfig = {
   appenders: {
-    database: { type: 'file', filename: 'logs/database.log' },
-    app: { type: 'file', filename: 'logs/app.log' },
-    console: { type: 'console' },
-    dev: {type: 'file', filename: 'logs/dev.log'},
-    received: {type: 'file', filename: 'logs/received.log'},
-    sent: {type: 'file', filename: 'logs/sent.log'},
-    error: {type: 'file', filename: 'logs/error.log'}
+    database: { type: "file", filename: "logs/database.log" },
+    app: { type: "file", filename: "logs/app.log" },
+    console: { type: "console" },
+    dev: { type: "file", filename: "logs/dev.log" },
+    received: { type: "file", filename: "logs/received.log" },
+    sent: { type: "file", filename: "logs/sent.log" },
+    error: { type: "file", filename: "logs/error.log" },
   },
   categories: {
-    default: { appenders: ['console'], level: 'info' },
-    app: { appenders: ['app', 'console'], level:  process.env.NODE_ENV == 'test' ? 'error' : 'info' },
-    database: { appenders: ['database'], level: 'info' },
+    default: { appenders: ["console"], level: "info" },
+    app: { appenders: ["app", "console"], level: process.env.NODE_ENV == "test" ? "error" : "info" },
+    database: { appenders: ["database"], level: "info" },
     dev: {
-      appenders: ['console', 'dev'],
-      level: ['developoment'].includes(process.env.NODE_ENV) ? 'trace' : 'off'
+      appenders: ["console", "dev"],
+      level: ["developoment"].includes(process.env.NODE_ENV) ? "trace" : "off",
     },
-    received: { appenders: ['received'], level: 'trace' },
-    sent: { appenders: ['sent'], level: 'trace' },
-    error: { appenders: ['error'], level: 'trace'}
-  }
+    received: { appenders: ["received"], level: "trace" },
+    sent: { appenders: ["sent"], level: "trace" },
+    error: { appenders: ["error"], level: "trace" },
+  },
 }
 log4js.configure(log4jsConfig)
-logging = {
+const logging = {
   log: (domain, level, message) => {
     const logger = log4js.getLogger(domain)
-    logger[level](message);
+    logger[level](message)
   },
   default: log4js.getLogger(),
-  database: log4js.getLogger('database'),
-  app: log4js.getLogger('app'),
-  dev: log4js.getLogger('dev'),
-  received: log4js.getLogger('received'),
-  sent: log4js.getLogger('sent'),
-  error: log4js.getLogger('error'),
+  database: log4js.getLogger("database"),
+  app: log4js.getLogger("app"),
+  dev: log4js.getLogger("dev"),
+  received: log4js.getLogger("received"),
+  sent: log4js.getLogger("sent"),
+  error: log4js.getLogger("error"),
   all: (level, message) => {
-    Object.keys(log4jsConfig.categories).map((category) => (log4js.getLogger(category)[level](message)));
-  }
+    Object.keys(log4jsConfig.categories).map((category) => log4js.getLogger(category)[level](message))
+  },
 }
 
 module.exports = logging

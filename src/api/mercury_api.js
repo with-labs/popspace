b59ff314799ed859722ddbc3e90afc90413041af
alexey-dc <alexey.cad@gmail.com>
2021-04-15 14:20:15 -0400

    update_participant_state log body as string

--- src/api/mercury_api.js
+++ src/api/mercury_api.js
@@ -196,10 +196,10 @@ class MercuryApi {
         return http.succeed(req, res, { participantState: req.body.participantState })
       } catch(e) {
         if(e.code == 'ProvisionedThroughputExceededException') {
-          log.error.error(`Dynamo throughput excededed: update participant state (user_id ${req.user.id}, body ${req.body}) ${e})`)
+          log.error.error(`Dynamo throughput excededed: update participant state (user_id ${req.user.id}, body ${JSON.stringify(req.body)}) ${e})`)
           return http.fail(req, res, shared.error.code.RATE_LIMIT_EXCEEDED, `Widget database write capacity temporarily exceeded, please retry`)
         } else {
-          log.error.error(`Unexpected error update participant state (user_id ${req.user.id}, body ${req.body}) ${e}`)
+          log.error.error(`Unexpected error update participant state (user_id ${req.user.id}, body ${JSON.stringify(req.body)}) ${e}`)
           return http.fail(req, res, shared.error.code.UNEXPECTER_ERROR, `Could not write widget to database, please try again later.`)
         }
       }

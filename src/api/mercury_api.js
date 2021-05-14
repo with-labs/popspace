9cf6963f2f15b251118bfc1079d263bb361ed0c7
jtyler <jesse@with.so>
2021-05-14 13:00:05 -0700

    removing consolelog

--- src/api/mercury_api.js
+++ src/api/mercury_api.js
@@ -175,7 +175,6 @@ class MercuryApi {
         return result
       }
 
-      console.log(`------- user : ${req.user.id}`)
       let defaultRoom = await shared.db.pg.massive.default_rooms.findOne({
         user_id: req.user.id,
       })

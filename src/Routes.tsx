92c87499ff35029249cb31e1254ef5da6820ab08
Grant Forrest <gaforres@gmail.com>
2021-05-26 10:46:33 -0400

    new create flow scaffold

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,7 +7,7 @@ import { LoginWithEmail } from './pages/LoginWithEmail/LoginWithEmail';
 import { VerifyEmail } from './pages/VerifyEmail/VerifyEmail';
 import { Subscribe } from './pages/Subscribe/Subscribe';
 import { Unsubscribe } from './pages/Unsubscribe/Unsubscribe';
-import { Route, Switch, useHistory } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AdminRoute } from './components/AdminRoute/AdminRoute';
 import { FlaggAdmin } from 'flagg/dist/react';
 import { Page } from './Layouts/Page/Page';
@@ -15,66 +15,17 @@ import { AuthenticatedRoute } from './components/AuthenticatedRoute/Authenticate
 import RoomPage from './pages/RoomPage/RoomPage';
 import { InviteLink } from './pages/InviteLink/InviteLink';
 import { Signup } from './pages/Signup/Signup';
-import { CreateRoomPage } from './pages/CreateRoom/CreateRoomPage';
-import { FullscreenLoading } from './components/FullscreenLoading/FullscreenLoading';
-import { useCurrentUserProfile } from './hooks/api/useCurrentUserProfile';
-import { useOrderedRooms } from './hooks/useOrderedRooms/useOrderedRooms';
-import useQueryParams from './hooks/useQueryParams/useQueryParams';
-import { MediaReadinessContext } from './components/MediaReadinessProvider/MediaReadinessProvider';
-
+import { Dashboard } from './pages/Dashboard/Dashboard';
 const LicensesPage = React.lazy(() => import('./pages/licenses/LicensesPage'));
 
 export interface IRoutesProps {}
 
-const RootView = () => {
-  const history = useHistory();
-  const { profile, error, isLoading: isProfileLoading } = useCurrentUserProfile();
-  const { rooms, isLoading: isRoomsLoading } = useOrderedRooms(profile);
-  const { resetReady } = React.useContext(MediaReadinessContext);
-
-  const query = useQueryParams();
-  const queryError = query.get('e');
-
-  React.useEffect(() => {
-    if (error) {
-      // todo: add error to query string here?
-      history.push(RouteNames.SIGN_IN);
-    }
-  }, [error, history]);
-
-  React.useEffect(() => {
-    if (queryError) {
-      // if we have an error on the query string,
-      // reset the ready is ready so we dont enter a room
-      // so the user can see the error
-      resetReady();
-    }
-
-    if (!isProfileLoading && !isRoomsLoading) {
-      // if the user has no rooms then we redirect to the onboarding
-      if (!error && rooms.length === 0) {
-        history.push(`${RouteNames.CREATE_ROOM}?onboarding=true`);
-      } else {
-        // hitting the route will redirect us to the first room in
-        // returned by useOrderedRooms.
-        // either their default room or the first room they have
-        // TOOD: expand this to use last entered room as the first choice?
-        history.replace({
-          pathname: `/${rooms[0].route}`,
-          search: `${history.location.search}`,
-        });
-      }
-    }
-  }, [history, rooms, isRoomsLoading, isProfileLoading, error, queryError, resetReady]);
-
-  return <FullscreenLoading />;
-};
-
 export const Routes: React.FC<IRoutesProps> = (props) => {
   return (
     <Switch>
+      {/* TODO: open this to anon users */}
       <AuthenticatedRoute exact path={RouteNames.ROOT}>
-        <RootView />
+        <Dashboard />
       </AuthenticatedRoute>
 
       <Route exact path={RouteNames.SIGN_IN}>
@@ -101,15 +52,13 @@ export const Routes: React.FC<IRoutesProps> = (props) => {
         <FinalizeAccount />
       </Route>
 
-      {/* 
+      {/*
         3/30/2021
         we are currently starting the transition from /invite -> /join
         so we will keep /invite availible for backward support.
       */}
       <Route path={RouteNames.INVITE} component={InviteLink} />
       <Route path={RouteNames.JOIN} component={InviteLink} />
-
-      <AuthenticatedRoute path={RouteNames.CREATE_ROOM} component={CreateRoomPage} />
 
       <AdminRoute path={RouteNames.ADMIN}>
         <Admin />
@@ -133,6 +82,7 @@ export const Routes: React.FC<IRoutesProps> = (props) => {
         </React.Suspense>
       </Route>
 
+      {/* TODO: open this to anon users */}
       <AuthenticatedRoute
         path="/:roomRoute"
         render={(renderProps) => {

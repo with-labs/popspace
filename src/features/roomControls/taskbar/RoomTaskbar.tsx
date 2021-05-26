9ee974ee312c9fcacfc194c482bc167590f2e07e
jtyler <jesse@with.so>
2021-05-25 17:08:57 -0700

    start of noodle ui, new widget menu

--- src/features/roomControls/taskbar/RoomTaskbar.tsx
+++ src/features/roomControls/taskbar/RoomTaskbar.tsx
@@ -2,20 +2,13 @@ import { Hidden, makeStyles, Paper, Theme, useMediaQuery } from '@material-ui/co
 import clsx from 'clsx';
 import { useFeatureFlag } from 'flagg';
 import * as React from 'react';
-import { isMacOs } from 'react-device-detect';
 import { useTranslation } from 'react-i18next';
-import { KeyShortcutText } from '../../../components/KeyShortcutText/KeyShortcutText';
 import { ResponsivePopoverProvider } from '../../../components/ResponsivePopover/ResponsivePopover';
-import { ResponsiveTooltip } from '../../../components/ResponsiveTooltip/ResponsiveTooltip';
 import { Spacing } from '../../../components/Spacing/Spacing';
 import { MediaFailedWrapper } from '../../../roomState/MediaFailedWrapper';
-import { WidgetType } from '../../../roomState/types/widgets';
 import { isPIPAvailable } from '../../pictureInPicture/pictureInPictureFeatureDetection';
 import { ACTION_BAR_ID } from '../addContent/ActionBar';
-import { ActionBarButton } from '../addContent/ActionBarButton';
 import { FloatingActionBarButton } from '../addContent/FloatingActionBarButton';
-import { QuickAccessoryButton } from '../addContent/QuickAccessoryButton';
-import { QuickFileButton } from '../addContent/QuickFileButton';
 import { PublishedCameraToggle } from '../media/PublishedCameraToggle';
 import { PublishedMicToggle } from '../media/PublishedMicToggle';
 import { PictureInPictureToggle } from '../media/PictureInPictureToggle';
@@ -85,16 +78,6 @@ export const RoomTaskbar: React.FC<IRoomTaskbarProps> = ({ className, ...rest })
             flex={1}
           >
             <MainMenu />
-            <Hidden smDown>
-              <ResponsiveTooltip title={<QuickActionTooltip />} offset={4}>
-                <ActionBarButton aria-label={t('features.omnibar.quickActionButton')} aria-controls={ACTION_BAR_ID} />
-              </ResponsiveTooltip>
-              <QuickAccessoryButton type={WidgetType.StickyNote} />
-              <QuickAccessoryButton type={WidgetType.Link} />
-              <QuickAccessoryButton type={WidgetType.Whiteboard} />
-              <QuickAccessoryButton type={WidgetType.YouTube} />
-              <QuickFileButton />
-            </Hidden>
           </Spacing>
           <MediaFailedWrapper>
             <Spacing gap={0.5} alignItems="center" color="grey.900" flexDirection={isHorizontal ? 'row' : 'column'}>
@@ -117,14 +100,5 @@ export const RoomTaskbar: React.FC<IRoomTaskbarProps> = ({ className, ...rest })
         />
       </Hidden>
     </>
-  );
-};
-
-const QuickActionTooltip = () => {
-  const { t } = useTranslation();
-  return (
-    <span>
-      {t('features.omnibar.quickActionButton')} <KeyShortcutText>{`${isMacOs ? '⌘' : 'Ctrl'} + K`}</KeyShortcutText>
-    </span>
   );
 };

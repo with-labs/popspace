efdf5b3c2ca22b56b3ee83b3e6609300b066f259
Jerry Zhu <jiqingjerry@gmail.com>
2021-06-07 13:53:48 -0700

    add post

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-require("./init/init_global")
+require("./init/init_global");
 
-const express = require("express")
-const app = express()
-const port = process.env.PORT
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send('non-api Hello World!'))
-app.get('/api/*', (req, res) => res.send('api Hello World!'))
+app.get('/', (req, res) => res.send('non-api Hello World!'));
+app.get('/api/*', (req, res) => res.send('api Hello World!'));
+app.post('/', (req, res) => res.send('post Hello World'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));

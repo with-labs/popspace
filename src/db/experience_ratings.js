125efbaecc0e48daede7b49264e1504db66ea577
Grant Forrest <gaforres@gmail.com>
2021-06-10 17:09:09 -0400

    update with actor naming

--- src/db/experience_ratings.js
+++ src/db/experience_ratings.js
@@ -2,25 +2,27 @@
  * @typedef {Object} ExperienceRating
  * @property {number} rating
  * @property {string | null} feedback
- * @property {string} submittedAt
- * @property {string} createdAt
- * @property {string} updatedAt
+ * @property {string} submitted_at
+ * @property {string} created_at
+ * @property {string} updated_at
  * @property {number} id
+ * @property {number} actor_id
+ * @property {number} room_id
  */
 
 class ExperienceRatings {
   /**
    * Creates a new experience rating entry
-   * @param {number} userId
+   * @param {number} actorId
    * @param {number} roomId
    * @param {number} rating 1-5
    * @param {Date} submittedAt
    * @param {string | null} feedback
    * @returns {Promise<ExperienceRating>}
    */
-  async createRating(userId, roomId, rating, submittedAt, feedback) {
+  async createRating(actorId, roomId, rating, submittedAt, feedback) {
     return shared.db.pg.massive.experience_ratings.insert({
-      user_id: userId,
+      actor_id: actorId,
       room_id: roomId,
       submitted_at: submittedAt.toUTCString(),
       rating,

ed4ad132e79d7748176235e4cc3d9fd56be73c1f
Grant Forrest <gaforres@gmail.com>
2021-06-18 12:40:45 -0400

    use a fullpage iframe for the landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,15 @@
-import { logger } from '@utils/logger';
-import * as React from 'react';
-
 export function LandingPage() {
-  React.useEffect(() => {
-    if (window.location.search.includes('ref=app')) {
-      // we reloaded and we're back here. Don't do an infinite reload loop...
-      // just navigate right to the landing page site.
-      logger.critical('Landing page redirect failed');
-      window.location.href = 'https://tilde.so?ref=app';
-    } else {
-      // reload this page with query ref=app
-      window.location.href = `${window.location.origin}?ref=app`;
-    }
-  }, []);
-  return null;
+  return (
+    <iframe
+      style={{
+        width: '100%',
+        height: '100%',
+        overflowY: 'auto',
+        flex: 1,
+        border: 'none',
+      }}
+      src="https://deploy-preview-3--stoic-mestorf-230354.netlify.app/"
+      title="The Tilde landing page"
+    />
+  );
 }

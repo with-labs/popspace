39e685eecf24bc6a0c5ca548c2cc928a4014babc
Grant Forrest <gaforres@gmail.com>
2021-06-22 12:04:03 -0400

    semi-working injected landing page

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const git = require('git-rev-sync');
 const CracoAlias = require('craco-alias');
+const CopyPlugin = require('copy-webpack-plugin');
 
 console.log('Customizing Webpack');
 module.exports = {
@@ -19,6 +20,22 @@ module.exports = {
       new webpack.DefinePlugin({
         VERSION: JSON.stringify(git.short()),
       }),
+      new CopyPlugin({
+        patterns: [
+          { context: 'node_modules/noodle-landing/dist/', from: '**/*', globOptions: { ignore: ['**/index.html'] } },
+        ],
+      }),
     ],
+    configure: (config, { env, paths }) => {
+      config.module.rules.push({
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          esModule: false,
+          attributes: false,
+        },
+      });
+      return config;
+    },
   },
 };
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,15 +1,21 @@
+import landingPageHtml from 'noodle-landing/dist/index.html';
+import Helmet from 'react-helmet';
+
+console.log(landingPageHtml);
+// const landingPageParsed = document.createElement('html');
+// landingPageParsed.innerHTML = landingPageHtml;
+// const landingPageHeadContent = landingPageParsed.getElementsByTagName('head').item(0)!.children;
+// const landingPageBodyContent = landingPageParsed.getElementsByTagName('body').item(0)!.children;
+
+const landingPageHeadContent = /<head>((?:.|\n)*)<\/head>/.exec(landingPageHtml)![1];
+const landingPageBodyContent = /<body>((?:.|\n)*)<\/body>/.exec(landingPageHtml)![1];
+
 export function LandingPage() {
   return (
-    <iframe
-      style={{
-        width: '100%',
-        height: '100%',
-        overflowY: 'auto',
-        flex: 1,
-        border: 'none',
+    <main
+      dangerouslySetInnerHTML={{
+        __html: `${landingPageHeadContent}${landingPageBodyContent}`,
       }}
-      src="https://tilde.so/"
-      title="The Tilde landing page"
-    />
+    ></main>
   );
 }

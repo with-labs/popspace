f19bedf9ff095a768a63cf9926ce7ef738fc50c1
Grant Forrest <gaforres@gmail.com>
2021-07-02 14:20:02 -0400

    responsive viewport controls

--- src/features/roomControls/RoomControls.tsx
+++ src/features/roomControls/RoomControls.tsx
@@ -4,31 +4,41 @@ import { ActionBar } from './addContent/ActionBar';
 import { WidgetMenu } from './widgetMenu/WidgetMenu';
 import { OnboardingPopup } from '@features/onboarding/OnboardingPopup';
 import { Spacing } from '@components/Spacing/Spacing';
-import { Box, Paper, useTheme } from '@material-ui/core';
+import { Box, makeStyles, Paper } from '@material-ui/core';
 import { CenterButton } from './viewport/CenterButton';
 import { ZoomInButton } from './viewport/ZoomInButton';
 import { ZoomOutButton } from './viewport/ZoomOutButton';
 
 export interface IRoomControlsProps {}
 
+const useStyles = makeStyles((theme) => ({
+  viewportControls: {
+    right: theme.spacing(3),
+    bottom: theme.spacing(3),
+    zIndex: theme.zIndex.speedDial,
+    position: 'fixed',
+    '@media(max-width: 600px)': {
+      bottom: 88,
+    },
+  },
+  zoomButtons: {
+    '@media(max-width: 600px)': {
+      display: 'none',
+    },
+  },
+}));
+
 export const RoomControls = React.memo<IRoomControlsProps>(() => {
-  const theme = useTheme();
+  const styles = useStyles();
 
   return (
     <>
       <RoomTaskbar />
-      <Spacing
-        gap={0.5}
-        flexDirection="column"
-        position="fixed"
-        zIndex={theme.zIndex.speedDial}
-        right={theme.spacing(3)}
-        bottom={theme.spacing(3)}
-      >
+      <Spacing gap={0.5} flexDirection="column" className={styles.viewportControls}>
         <Box component={Paper} padding={0.25}>
           <CenterButton />
         </Box>
-        <Spacing component={Paper} padding={0.25} gap={0.25} flexDirection="column">
+        <Spacing component={Paper} padding={0.25} gap={0.25} flexDirection="column" className={styles.zoomButtons}>
           <ZoomInButton />
           <ZoomOutButton />
         </Spacing>

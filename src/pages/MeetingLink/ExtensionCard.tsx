e3f5dd656f275c733568336b53c0e9bde1359613
Jerry Zhu <jiqingjerry@gmail.com>
2021-07-06 15:14:22 -0700

    added i18n entries for extension buttons

--- src/pages/MeetingLink/ExtensionCard.tsx
+++ src/pages/MeetingLink/ExtensionCard.tsx
@@ -3,6 +3,7 @@ import { Spacing } from '@components/Spacing/Spacing';
 import * as React from 'react';
 import { snow } from '@src/theme/theme';
 import { SaveIcon } from '@components/icons/SaveIcon';
+import { useTranslation } from 'react-i18next';
 
 export interface IExtensionCardProps {
   iconSrc: string;
@@ -19,13 +20,14 @@ export const ExtensionCard: React.FC<IExtensionCardProps> = ({
   disabled = false,
   onClick,
 }) => {
+  const { t } = useTranslation();
   return (
     <Spacing component={Card} p={2} flexDirection="column" alignItems="center">
       <img width={48} height={48} src={iconSrc} alt={iconAlt} />
       <Typography variant="body2">{label}</Typography>
       <ThemeProvider theme={snow}>
         <Button color="default" disabled={disabled} onClick={onClick} startIcon={!disabled ? <SaveIcon /> : null}>
-          {!disabled ? 'Install' : 'Coming Soon'}
+          {t(`pages.meetingLink.extensions.${!disabled ? 'install' : 'comingSoon'}`)}
         </Button>
       </ThemeProvider>
     </Spacing>

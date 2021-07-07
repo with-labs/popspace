3d9e79aa2bfa25f36924127faccdf7498d218521
Grant Forrest <gaforres@gmail.com>
2021-07-07 16:16:18 -0400

    partially mitigate xss vuln in link widget. refactor. fix sizing to be responsive.

--- src/features/room/widgets/link/constants.ts
+++ src/features/room/widgets/link/constants.ts
@@ -1,24 +1,19 @@
 export const SIZE_STUB = { width: 300, height: 80 };
 
 export const INITIAL_SIZE_FRAME = { width: 500, height: 500 };
-export const MIN_SIZE_FRAME = { width: 300, height: 200 };
+export const MIN_SIZE_FRAME = { width: 320, height: 250 };
 export const MAX_SIZE_FRAME = { width: 2000, height: 2000 };
-
-// media widgets measure their own size from content, but there's
-// still a limit to how large they are made initially
-export const MAX_INITIAL_SIZE_MEDIA = { width: 800, height: 800 };
-export const MIN_SIZE_MEDIA = { width: 80, height: 54 };
-export const MAX_SIZE_MEDIA = { width: 4000, height: 4000 };
-
-export const INITIAL_SIZE_FRAME_MEDIA = { width: 340, height: 94 };
-export const MIN_SIZE_FRAME_MEDIA = { width: 240, height: 94 };
-export const MAX_SIZE_FRAME_MEDIA = { width: 4000, height: 4000 };
 
 export const INITIAL_SIZE_EDIT = { width: 240, height: 200 };
 export const MIN_SIZE_EDIT = { width: 240, height: 200 };
 export const MAX_SIZE_EDIT = { width: 400, height: 300 };
 
-// link widgets default dimensions
-export const LINK_SUMMARY_WIDTH = 500;
-export const DEFAULT_IFRAME = { width: 500, height: 500 };
-export const WIDGET_TITLE_HEIGHT = 48;
+export const SUMMARY_SIZE = {
+  width: 500,
+  height: 300,
+};
+
+export const SUMMARY_WITH_IMAGE_SIZE = {
+  width: 500,
+  height: 440,
+};

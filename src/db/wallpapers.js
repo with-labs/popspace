0213c0437330e136bd9cc1411c375d2a00c78c02
Grant Forrest <gaforres@gmail.com>
2021-08-02 14:57:57 -0400

    add method for wallpaper access

--- src/db/wallpapers.js
+++ src/db/wallpapers.js
@@ -4,6 +4,11 @@ class Wallpapers {
   getWallpapersForActor = (actorId) => {
     return shared.db.pg.massive.wallpapers.where('creator_id IN ($1, $2)', [actorId, SYSTEM_USER_ID]);
   }
+
+  canUserAccessWallpaper = (actorId, wallpaperId) => {
+    const wallpaper = shared.db.pg.massive.wallpapers.findOne(wallpaperId);
+    return (wallpaper && (wallpaper.creator_id === actorId || wallpaper.creator_id === SYSTEM_USER_ID));
+  }
 }
 
 module.exports = new Wallpapers();

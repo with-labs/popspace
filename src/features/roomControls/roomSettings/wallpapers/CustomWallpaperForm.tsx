6a695f1586b74311fb06a4ceee2968cc6f73d0cd
Grant Forrest <gaforres@gmail.com>
2021-08-04 12:36:06 -0400

    improve loading experience

--- src/features/roomControls/roomSettings/wallpapers/CustomWallpaperForm.tsx
+++ src/features/roomControls/roomSettings/wallpapers/CustomWallpaperForm.tsx
@@ -1,22 +1,17 @@
 import client from '@api/client';
 import { FileUploadButton } from '@components/FileUploadButton/FileUploadButton';
 import { Spacing } from '@components/Spacing/Spacing';
-import { Button, makeStyles } from '@material-ui/core';
+import { Button, CircularProgress } from '@material-ui/core';
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 
 import { useWallpapers } from './useWallpapers';
 
 export interface ICustomWallpaperFormProps {}
 
-const useStyles = makeStyles((theme) => ({
-  formWrapper: {},
-  submitBtn: {},
-}));
-
 export const CustomWallpaperForm: React.FC<ICustomWallpaperFormProps> = () => {
   const { t } = useTranslation();
-  const classes = useStyles();
 
   const [file, setFile] = useState<File | null>(null);
   const onFileUpload = (files: File[]) => {
@@ -24,24 +19,27 @@ export const CustomWallpaperForm: React.FC<ICustomWallpaperFormProps> = () => {
   };
 
   const [_, uploadWallpaper] = useWallpapers();
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async () => {
     if (!file) return;
-    // upload to the API server
-    const wallpaper = await uploadWallpaper(file);
-    client.wallpapers.setWallpaper(wallpaper);
+    setLoading(true);
+    try {
+      // upload to the API server
+      const wallpaper = await uploadWallpaper(file);
+      client.wallpapers.setWallpaper(wallpaper);
+    } catch (e) {
+      toast.error(t('error.api.UNEXPECTED.message') as string);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <Spacing className={classes.formWrapper} flexDirection="column">
+    <Spacing flexDirection="column">
       <FileUploadButton onChange={onFileUpload} accept=".PNG, .JPG, .JPEG, .WebP" />
-      <Button
-        disabled={!file}
-        className={classes.submitBtn}
-        data-testid="custom-wallpaper-submit-button"
-        onClick={onSubmit}
-      >
-        {t('features.room.submitButtonText')}
+      <Button disabled={!file || loading} data-testid="custom-wallpaper-submit-button" onClick={onSubmit}>
+        {loading ? <CircularProgress size="22px" color="inherit" /> : t('features.room.submitButtonText')}
       </Button>
     </Spacing>
   );

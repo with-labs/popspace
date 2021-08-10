fe09fd93341e69fe4e9b9f43cc2ae46f5b43c988
Grant Forrest <gaforres@gmail.com>
2021-08-10 13:42:02 -0400

    syntax update

--- src/db/wallpapers.js
+++ src/db/wallpapers.js
@@ -1,11 +1,11 @@
 const SYSTEM_USER_ID = '-5000';
 
 class Wallpapers {
-  getWallpapersForActor = (actorId) => {
+  getWallpapersForActor(actorId) {
     return shared.db.pg.massive.wallpapers.where('creator_id IN ($1, $2)', [actorId, SYSTEM_USER_ID]);
   }
 
-  canUserAccessWallpaper = async (actorId, wallpaperId) => {
+  async canUserAccessWallpaper(actorId, wallpaperId) {
     const wallpaper = await shared.db.pg.massive.wallpapers.findOne(wallpaperId);
     return (wallpaper && (wallpaper.creator_id === actorId || wallpaper.creator_id === SYSTEM_USER_ID));
   }

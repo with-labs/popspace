76dff27f538ceb6beaec1d9a2a62fefe715f7a7d
Grant Forrest <gaforres@gmail.com>
2021-09-01 14:56:14 -0400

    add survey API endpoints

---
+++ src/api/endpoints/surveys.js
@@ -0,0 +1,51 @@
+const { default: shared } = require("@withso/noodle-shared");
+
+class Surveys {
+  constructor(zoo) {
+    zoo.loggedInPostEndpoint("/respond_to_survey", this.handleRespondToSurvey.bind(this), ['surveyName', 'response'], []);
+    zoo.loggedInGetEndpoint("/survey_responses", this.getSurveyResponses.bind(this), [], []);
+    zoo.loggedInGetEndpoint("/survey_response", this.getSurveyResponse.bind(this), ['surveyName'], []);
+  }
+
+  async handleRespondToSurvey(req, res) {
+    const { surveyName, response } = req.body;
+    const existingResponse = await shared.db.prisma.surveyResponse.findFirst({
+      where: {
+        actorId: req.actor.id,
+        surveyName,
+      }
+    })
+    if (existingResponse) {
+      return api.http.fail(req, res, { message: 'You have already responded to this survey' }, 400);
+    }
+    const newResponse = await shared.db.prisma.surveyResponse.create({
+      data: {
+        actorId: req.actor.id,
+        surveyName,
+        response,
+      }
+    });
+    lib.feedback.notifySurveyResponse(newResponse, req.actor)
+    return api.http.succeed(req, res, { response: newResponse });
+  }
+
+  async getSurveyResponses(req, res) {
+    const responses = await shared.db.prisma.surveyResponse.findMany({
+      where: {
+        actorId: req.actor.id,
+      }
+    })
+    return api.http.succeed(req, res, { responses });
+  }
+
+  async getSurveyResponse(req, res) {
+    const { surveyName } = req.params;
+    const response = await shared.db.prisma.surveyResponse.findFirst({
+      where: {
+        actorId: req.actor.id,
+        surveyName,
+      }
+    })
+    return api.http.succeed(req, res, { response });
+  }
+}

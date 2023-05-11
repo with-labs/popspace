7c52c35e7b323a6f36c9c83eb59ed82ecd69f48f
Grant Forrest <gaforres@gmail.com>
2023-05-11 11:13:10 -0400

    move unicorn health check to better spot

--- unicorn/app/api/express_next_router.js
+++ unicorn/app/api/express_next_router.js
@@ -1,40 +1,41 @@
-
-
 class Router {
   constructor(express, next) {
-    this.express = express
-    this.next = next
+    this.express = express;
+    this.next = next;
   }
 
   async init() {
-    this.initApi()
-    this.initNextJsRoutes()
-    this.initErrors()
+    this.express.get('/health', (req, res) => {
+      res.status(200).send('OK');
+    });
+    this.initApi();
+    this.initNextJsRoutes();
+    this.initErrors();
   }
 
   initApi() {
-    (new (require('./endpoints'))(this.express)).init()
+    new (require('./endpoints'))(this.express).init();
   }
 
   initNextJsRoutes() {
-    (new (require("./pages"))(this.express, this.next)).init()
+    new (require('./pages'))(this.express, this.next).init();
   }
 
   initErrors() {
     // catch 404 and forward to error handler
     this.express.use((req, res, next) => {
-      const err = new Error('Not Found')
-      err.status = 404
-      next(err)
-    })
+      const err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
 
     this.express.use((err, req, res, next) => {
-      res.status(err.status || 500)
-      res.locals.error = err
-      res.locals.errorDescription = err.message
-      this.next.render(req, res, "/_error", {  })
-    })
+      res.status(err.status || 500);
+      res.locals.error = err;
+      res.locals.errorDescription = err.message;
+      this.next.render(req, res, '/_error', {});
+    });
   }
 }
 
-module.exports = Router
+module.exports = Router;
--- unicorn/app/api/pages.js
+++ unicorn/app/api/pages.js
@@ -13,10 +13,6 @@ class Pages {
       return this.next.render(req, res, `/main`, req.query);
     });
 
-    this.express.get('/health', (req, res) => {
-      res.status(200).send('OK');
-    });
-
     this.express.get('*', (req, res) => {
       return this.next.render(req, res, `/${req.path}`, req.query);
     });
